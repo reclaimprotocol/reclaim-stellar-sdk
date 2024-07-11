c2f7ed14f2de22c738d9ee5aaa06a03fdc99d4a5
hadi <hadi@creatoros.co>
2024-07-11 16:45:24 +0300

    setup reclaim

--- contracts/reclaim/src/lib.rs
+++ contracts/reclaim/src/lib.rs
@@ -1,14 +1,149 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, symbol_short, vec, Env, Symbol, Vec};
+use soroban_sdk::{
+    contract, contracterror, contractimpl, contracttype, symbol_short, vec, Address, BytesN, Env,
+    String, Symbol, Vec,
+};
 
 #[contract]
 pub struct ReclaimContract;
 
+const CONFIG: Symbol = symbol_short!("CONFIG");
+const EPOCH: Symbol = symbol_short!("EPOCH");
+
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Config {
+    pub owner: Address,
+    pub current_epoch: u128,
+    pub exists: bool,
+}
+
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Witness {
+    pub address: BytesN<20>,
+    pub host: String,
+}
+
+impl Witness {
+    pub fn get_addresses(witness: Vec<Witness>) -> Vec<BytesN<20>> {
+        let env = Env::default();
+        let mut vec_addresses = vec![&env];
+        for wit in witness {
+            vec_addresses.push_back(wit.address);
+        }
+        return vec_addresses;
+    }
+}
+
+#[contracttype]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Epoch {
+    pub id: u128,
+    pub timestamp_start: u64,
+    pub timestamp_end: u64,
+    pub minimum_witness: u128,
+    pub witnesses: Vec<Witness>,
+}
+
+fn generate_random_seed(bytes: Vec<u8>, offset: usize) -> u32 {
+    let hash_slice = &bytes.slice(offset as u32..(offset + 4) as u32);
+    let mut seed = 0u32;
+
+    // for i in 0..hash_slice.len(){
+    //     let byte = hash_slice.x;
+    // }
+
+    // for (i, &byte) in hash_slice.iter().enumerate() {
+    //     seed |= u32::from(byte) << (i * 8);
+    // }
+
+    seed
+}
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum ReclaimError {
+    OnlyOwner = 1,
+    AlreadyInitialized = 2,
+    HashMismatch = 3,
+    LengthMismatch = 4,
+    SignatureMismatch = 5,
+}
+
 #[contractimpl]
 impl ReclaimContract {
-    pub fn hello(env: Env, to: Symbol) -> Vec<Symbol> {
-        vec![&env, symbol_short!("Hello"), to]
+    pub fn instantiate(env: Env, user: Address) -> Result<(), ReclaimError> {
+        user.require_auth();
+
+        let default_config = Config {
+            owner: user.clone(),
+            current_epoch: 0_u128,
+            exists: false,
+        };
+        let mut config: Config = env
+            .storage()
+            .persistent()
+            .get(&CONFIG)
+            .unwrap_or(default_config);
+
+        if config.exists == true {
+            return Err(ReclaimError::AlreadyInitialized);
+        }
+
+        config = Config {
+            owner: user,
+            current_epoch: 0_u128,
+            exists: true,
+        };
+
+        env.storage().persistent().set(&CONFIG, &config);
+
+        let now = env.ledger().timestamp();
+        let epoch = Epoch {
+            id: 0_u128,
+            timestamp_start: now,
+            timestamp_end: now + 10000_u64,
+            minimum_witness: 1_u128,
+            witnesses: vec![&env],
+        };
+
+        env.storage().persistent().set(&EPOCH, &epoch);
+        Ok(())
     }
+
+    pub fn add_epoch(
+        env: Env,
+        witnesses: Vec<Witness>,
+        minimum_witness: u128,
+    ) -> Result<(), ReclaimError> {
+        let mut config: Config = env.storage().persistent().get(&CONFIG).unwrap();
+        let admin = config.owner.clone();
+        admin.require_auth();
+
+        let new_epoch_id = config.current_epoch + 1_u128;
+        let now = env.ledger().timestamp();
+
+        let epoch = Epoch {
+            id: new_epoch_id,
+            timestamp_start: now,
+            timestamp_end: now + 10000_u64,
+            minimum_witness,
+            witnesses: witnesses,
+        };
+
+        env.storage().persistent().set(&EPOCH, &epoch);
+
+        config.current_epoch = new_epoch_id;
+        env.storage().persistent().set(&CONFIG, &config);
+
+        Ok(())
+    }
+
+    // pub fn verify_proof(env: Env, signed_claim: SignedClaim) -> Result<(), ReclaimError> {
+    //     Ok(())
+    // }
 }
 
 mod test;
--- contracts/reclaim/src/test.rs
+++ contracts/reclaim/src/test.rs
@@ -1,17 +1,44 @@
 #![cfg(test)]
 
 use super::*;
-use soroban_sdk::{symbol_short, vec, Env};
+use soroban_sdk::{symbol_short, testutils::Address, vec, Env};
 
 #[test]
-fn test() {
+fn should_initialize() {
     let env = Env::default();
+    env.mock_all_auths();
+
+    let contract_id = env.register_contract(None, ReclaimContract);
+    let client = ReclaimContractClient::new(&env, &contract_id);
+    let user = <soroban_sdk::Address as Address>::generate(&env);
+
+    assert_eq!(client.instantiate(&user), ());
+}
+
+#[test]
+fn should_add_epoch() {
+    let env = Env::default();
+    env.mock_all_auths();
+
     let contract_id = env.register_contract(None, ReclaimContract);
     let client = ReclaimContractClient::new(&env, &contract_id);
 
-    let words = client.hello(&symbol_short!("Dev"));
-    assert_eq!(
-        words,
-        vec![&env, symbol_short!("Hello"), symbol_short!("Dev"),]
-    );
+    let user = <soroban_sdk::Address as Address>::generate(&env);
+    assert_eq!(client.instantiate(&user), ());
+
+    let mut witnesses = Vec::new(&env);
+
+    let bytes = "244897572368eadf65bfbc5aec98d8e5443a9072".as_bytes();
+
+    let items = &bytes[0..20]
+        .try_into()
+        .expect("slice with incorrect length");
+
+    let witness = Witness {
+        address: BytesN::<20>::from_array(&env, items),
+        host: String::from_str(&env, "http"),
+    };
+    witnesses.push_back(witness);
+
+    assert_eq!(client.add_epoch(&witnesses, &1_u128), ());
 }
